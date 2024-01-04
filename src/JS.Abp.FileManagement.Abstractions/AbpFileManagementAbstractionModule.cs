b2a157f6bb9f48d87404452424b33c41e251fab9
zhaofenglee <zhaof_li@163.com>
2024-01-04 20:32:55 +0800

    Add FileManagement.Abstractions.

---
+++ src/JS.Abp.FileManagement.Abstractions/AbpFileManagementAbstractionModule.cs
@@ -0,0 +1,14 @@
+﻿using Volo.Abp.EventBus;
+using Volo.Abp.Modularity;
+using Volo.Abp.MultiTenancy;
+
+namespace JS.Abp.FileManagement.Abstractions;
+
+[DependsOn(
+    typeof(AbpMultiTenancyModule),
+    typeof(AbpEventBusModule)
+)]
+public class AbpFileManagementAbstractionModule:AbpModule
+{
+    
+}
---
+++ src/JS.Abp.FileManagement.Abstractions/IFileData.cs
@@ -0,0 +1,19 @@
+﻿using Volo.Abp.Data;
+
+namespace JS.Abp.FileManagement.Abstractions;
+
+public interface IFileData : IHasExtraProperties
+{
+    Guid? TenantId { get; }
+    Guid ProjectId { get; }
+    Guid FileDescriptorId { get; }
+    Guid DirectoryId { get; }
+    string? Project { get; }
+    string Name { get; }
+    int Size { get; }
+    string? MimeType { get; }
+    
+    Guid? CreateUser { get; }
+    //是否使用
+    //bool IsUsed { get; }
+}
