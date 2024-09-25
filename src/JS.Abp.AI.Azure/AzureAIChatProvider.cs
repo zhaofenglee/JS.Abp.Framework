b3b96d9025098a3671779fe6356b7248c22189bf
zhaofenglee <zhaof_li@163.com>
2024-09-25 19:08:11 +0800

    Refactor AzureAIChatProvider to use Azure.AI.OpenAI for chat functionality

--- src/JS.Abp.AI.Azure/AzureAIChatProvider.cs
+++ src/JS.Abp.AI.Azure/AzureAIChatProvider.cs
@@ -1,6 +1,6 @@
 ﻿using Azure;
-using Azure.AI.Inference;
-using JS.Abp.AI.DashScope;
+using Azure.AI.OpenAI;
+using OpenAI.Chat;
 using Volo.Abp.DependencyInjection;
 using Volo.Abp.Json;
 
@@ -23,31 +23,31 @@ public class AzureAIChatProvider:ChatProviderBase,ITransientDependency
     {
         var endpoint = new Uri(await AzureSettingConfiguration.GetEndpointAsync());
         var credential = new AzureKeyCredential(await AzureSettingConfiguration.GetApiKeyAsync());
+        AzureOpenAIClient azureClient = new(
+            endpoint,
+            credential);
 
-        var client = new ChatCompletionsClient(endpoint, credential, new ChatCompletionsClientOptions());
-        List<ChatRequestMessage> chatMessage = new List<ChatRequestMessage>();
+        var client =  azureClient.GetChatClient(modelId);
+        List<ChatMessage> chatMessage = new List<ChatMessage>();
         foreach (var message in chatMessages)
         {
             if (message.Role.ToLower() == "user")
             {
-                chatMessage.Add(new ChatRequestUserMessage(message.Content));
+                chatMessage.Add(new UserChatMessage(message.Content));
             }
             else
             {
-                chatMessage.Add(new ChatRequestSystemMessage(message.Content));
+                chatMessage.Add(new SystemChatMessage(message.Content));
             }
         }
 
-        var requestOptions = new ChatCompletionsOptions(
-            messages: chatMessage
-        )
+        var chatCompletionOptions = new ChatCompletionOptions ()
         {
-            Model = modelId,
             Temperature = 1,
-            MaxTokens = maxTokens
+            MaxOutputTokenCount = maxTokens
         };
 
-        Response<ChatCompletions> response = await client.CompleteAsync(requestOptions);
-        return response.Value.Choices[0].Message.Content;
+        ChatCompletion completion  = await client.CompleteChatAsync(chatMessage, chatCompletionOptions,cancellationToken);
+        return completion.Content[0].Text;
     }
 }
